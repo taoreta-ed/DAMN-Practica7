188bb7f3adce1360fd2348d6d44cbd7c72ea2d1c
Eduardo Castro <nora.castroeduardo@gmail.com>
2025-07-01 09:55:50 -0600

    Integrate Firebase and add .gitignore for google-services.json
    This commit adds the Google Services plugin and Firebase Analytics dependencies to the Android project. It also updates the .gitignore file to exclude the `app/google-services.json` file.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -36,6 +38,9 @@ android {
 }
 
 dependencies {
+
+    implementation(platform("com.google.firebase:firebase-bom:33.16.0"))
+    implementation("com.google.firebase:firebase-analytics")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
